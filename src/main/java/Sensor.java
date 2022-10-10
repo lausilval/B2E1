4762bdf2c8832c036922fe2c5d6fb81bb0cda655
lausilval <laura.silva.loureiro@rai.usc.es>
2022-10-10 10:07:24 +0200

    version1

--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -1,2 +1,23 @@
-public class Sensor {
+import java.util.ArrayList;
+
+public class Sensor
+{
+    // ATRIBUTOS
+    String id; //identificador del sensor
+    String tipo; // tipo de sensor
+    //Estancia estancia; //nombre de la estancia donde esta el sensor
+    ArrayList<Float> datos; //conjunto de datos que ha capturado el sensor
+    Float[] rango; //rango de valores que puede tomar el sensor
+    Float precio; // precio del sensor
+
+
+    // CONSTRUCTORES
+    public Sensor(String id, String tipo, float[] rango, float precio)
+    {
+        if(id != null)
+        {
+            this.id = id;
+        }
+    }
+
 }
